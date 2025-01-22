71451957560384bf1ae7793b5f7ba7febf8ea471
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-22 11:58:43 +0200

    Read aws role instead of creating new one

--- main.tf
+++ main.tf
@@ -12,15 +12,14 @@ data "aws_iam_policy_document" "assume_role"{
 }
 
 
-data "aws_iam_role" "iam_for_lambda" {
+data "aws_iam_role" "lambda_user" {
   name = "lambda-user"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function.zip"
   function_name = "lambda_handler"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = data.aws_iam_role.lambda_user.arn
   handler       = "lambda_function.lambda_handler"
 
   source_code_hash = filebase64sha256("lambda_function.zip")
