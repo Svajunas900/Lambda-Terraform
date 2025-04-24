f5e913d4267f619ad5cee710d47639016ce6e4ae
Svajunas900 <svajunaskoncius11@gmail.com>
2025-04-24 12:27:58 +0300

    Test

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ import requests
 def my_lambda_handler(event, context):
     url = event["url"]
     response = requests.get(url)
+
     return {
         'response': response.status_code,
         'response text': response.text
